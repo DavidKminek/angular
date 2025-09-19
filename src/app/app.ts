9307f68fdd5cb14448db0e8c482dc008fb44a043
KminekD23 <KminekD23@Z00T6-05>
2025-09-19 12:17:59 +0200

    header a popis a obrazok

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>ADAM MAJAK RAKOVA</h1>
-  `,
-  styleUrls: ['./app.css'],
+  standalone: true,
+  imports: [HeaderComponent, RouterOutlet], 
+  templateUrl: './app.html', 
+  styleUrls: ['./app.css']
 })
-export class App {
-  title = 'default';
-}
+export class AppComponent {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
-/*
- *  Protractor support is deprecated in Angular.
- *  Protractor is used in this example for compatibility with Angular documentation tools.
- */
-import {bootstrapApplication, provideProtractorTestingSupport} from '@angular/platform-browser';
-import {App} from './app/app';
+// main.ts
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideProtractorTestingSupport } from '@angular/platform-browser';
+import { AppComponent } from './app/app'; // Corrected
 
-bootstrapApplication(App, {providers: [provideProtractorTestingSupport()]}).catch((err) =>
-  console.error(err),
-);
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport()],
+}).catch((err) => console.error(err));
