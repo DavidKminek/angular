2f786bb4243567b3b529ad71201bcce5315b5c6e
KminekD23 <KminekD23@Z00T6-05>
2025-09-26 13:42:57 +0200

    uloha 5, 6, 7

---
+++ src/app/quests/quests-item.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+
+export interface Quest {
+  id: number;
+  title: string;
+  description: string;
+  xp: number;
+}
+
+@Component({
+  selector: 'app-quest-item',
+  standalone: true,
+  templateUrl: './quest-item.html',
+  styleUrls: ['./quest-item.css']
+})
+export class QuestItem {
+  quest = input.required<Quest>();      
+  index = input.required<number>(); 
+}
--- src/app/quests/quests.ts
+++ src/app/quests/quests.ts
@@ -1,22 +1,24 @@
 import { Component } from '@angular/core';
+import { QuestItem } from './quests-item';
 
-type Quest = {
+export interface Quest {
+  id: number;
   title: string;
   description: string;
-  completed: boolean;
-};
+  xp: number;
+}
 
 @Component({
   selector: 'app-quests',
   standalone: true,
-  imports: [],
+  imports: [QuestItem],
   templateUrl: './quests.html',
   styleUrls: ['./quests.css']
 })
 export class Quests {
   quests: Quest[] = [
-    { title: 'Find the Lost Sword', description: 'Retrieve the legendary sword from the ancient ruins.', completed: false },
-    { title: 'Rescue the Villagers', description: 'Save the villagers captured by goblins.', completed: true },
-    { title: 'Collect Herbs', description: 'Gather 10 healing herbs for the village healer.', completed: false }
+    { id: 1, title: 'Find the Lost Sword', description: 'Retrieve the legendary sword from the ancient ruins.', xp: 40 },
+    { id: 2, title: 'Rescue the Villagers', description: 'Save the villagers captured by goblins.', xp: 120 },
+    { id: 3, title: 'Collect Herbs', description: 'Gather 10 healing herbs for the village healer.', xp: 60 }
   ];
 }
